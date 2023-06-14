2df9831a0b063c3e3f38114696f57789cf283bb7
Muhammad Multazam <multazam@atech.software>
2023-06-14 11:43:56 +0700

    setup ci cd with docker

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: '.next',
-  future: {
-    webpack5: true,
-  },
-  concurrentBuild: true,
 }
 
 module.exports = nextConfig
